ce8c916d439cc427c0170fd229d5f48158d0bd95
RasmusElmersson <126062642+RasmusElmersson@users.noreply.github.com>
2024-11-18 17:40:06 +0100

    feat: error and oauth

--- src/auth.ts
+++ src/auth.ts
@@ -14,6 +14,14 @@ export const {
     signIn: "/login",
     error: "/error",
   },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
   callbacks: {
     async session({ token, session }) {
       if (token.sub && session.user) {
--- src/components/forms/login/index.tsx
+++ src/components/forms/login/index.tsx
@@ -6,6 +6,7 @@ import { type LoginFormValues, LoginSchema } from "./schema";
 import FormItem from "@/components/global/form-inputfield";
 import { useState, useTransition } from "react";
 import { login } from "@/actions/login";
+import { OAuthButtons } from "@/components/global/oauth-buttons";
 
 const LoginForm = () => {
   const [error, setError] = useState<string | undefined>(undefined);
@@ -25,27 +26,30 @@ const LoginForm = () => {
     });
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-      <FormItem
-        label={"Email"}
-        controlProps={{
-          name: "email",
-          control: control,
-        }}
-      />
-      <FormItem
-        label={"Password"}
-        type="password"
-        controlProps={{
-          name: "password",
-          control: control,
-        }}
-      />
-      <Button className="w-full" loading={isPending}>
-        Login
-      </Button>
-      {error && <p className="text-red-400">{error}</p>}
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <FormItem
+          label={"Email"}
+          controlProps={{
+            name: "email",
+            control: control,
+          }}
+        />
+        <FormItem
+          label={"Password"}
+          type="password"
+          controlProps={{
+            name: "password",
+            control: control,
+          }}
+        />
+        <Button className="w-full" loading={isPending}>
+          Login
+        </Button>
+        {error && <p className="text-red-400">{error}</p>}
+      </form>
+      <OAuthButtons />
+    </>
   );
 };
 
