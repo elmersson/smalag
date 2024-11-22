fe6086c3ca4ead17b29cf8802679e688dd4c443a
elmersson <elmerssonrasmus@gmail.com>
2024-11-22 07:51:12 +0100

    feat: two-factor-verification (#17)

--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -1,8 +1,34 @@
 import { v4 as uuidv4 } from "uuid";
+import crypto from "node:crypto";
 
 import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { db } from "./db";
 import { getPasswordResetTokenByEmail } from "@/data/password-token";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
+
+export const generateTwoFactorToken = async (email: string) => {
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000);
+  const existingToken = await getTwoFactorTokenByEmail(email);
+
+  if (existingToken) {
+    await db.twoFactorToken.delete({
+      where: {
+        id: existingToken.id,
+      },
+    });
+  }
+
+  const twoFactorToken = await db.twoFactorToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return twoFactorToken;
+};
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
