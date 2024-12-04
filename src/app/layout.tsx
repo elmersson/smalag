437d86746b6d7aa8bfd0b427972d41c2cd3ff03b
RasmusElmersson <126062642+RasmusElmersson@users.noreply.github.com>
2024-12-04 19:10:53 +0100

    feat: tanstack, axios and query for spaces

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Plus_Jakarta_Sans } from "next/font/google";
 import { Toaster } from "sonner";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
+import { ReactQueryClientProvider } from "@/providers/query/ReactQueryClientProvider";
 
 const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
@@ -23,18 +24,20 @@ export default async function RootLayout({
 
   return (
     <SessionProvider session={session}>
-      <html lang="en" suppressHydrationWarning>
-        <body className={`${jakarta.className} bg-black`}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            disableTransitionOnChange
-          >
-            {children}
-            <Toaster richColors />
-          </ThemeProvider>
-        </body>
-      </html>
+      <ReactQueryClientProvider>
+        <html lang="en" suppressHydrationWarning>
+          <body className={`${jakarta.className} bg-black`}>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              disableTransitionOnChange
+            >
+              {children}
+              <Toaster richColors />
+            </ThemeProvider>
+          </body>
+        </html>
+      </ReactQueryClientProvider>
     </SessionProvider>
   );
 }
