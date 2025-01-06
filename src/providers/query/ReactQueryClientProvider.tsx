51f0a39b2b543e6587837ec2ccde54a7c51b89c6
elmersson <elmerssonrasmus@gmail.com>
2025-01-06 17:58:14 +0100

    feat: settings for query (#39)

--- src/providers/query/ReactQueryClientProvider.tsx
+++ src/providers/query/ReactQueryClientProvider.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React, { useState } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import type React from "react";
+import { useState } from "react";
 
 export const ReactQueryClientProvider = ({
   children,
@@ -13,14 +15,20 @@ export const ReactQueryClientProvider = ({
       new QueryClient({
         defaultOptions: {
           queries: {
-            // With SSR, we usually want to set some default staleTime
-            // above 0 to avoid refetching immediately on the client
-            staleTime: 60 * 1000,
+            refetchOnWindowFocus: false,
+            retry: 2,
+            gcTime: 300000, // Cache data for 5 minutes (in milliseconds)
+            staleTime: 120000, // Keep stale data for 2 minutes (in milliseconds)
+            retryDelay: (attemptIndex) =>
+              Math.min(1000 * 2 ** attemptIndex, 30000), // Retry after 1 second, then 2, 4, 8, 16, 32, 64 seconds, etc...
           },
         },
       }),
   );
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
