b7115e69309e913cc7a992ead143b919856a6da0
RasmusElmersson <126062642+RasmusElmersson@users.noreply.github.com>
2025-01-20 21:21:55 +0100

    feat: linear integration fetch issues

--- src/app/(landing)/backlog/page.tsx
+++ src/app/(landing)/backlog/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
 import GradientText from "@/components/global/gradient-text";
-import { BacklogTable } from "./_components/backlog-tabble";
+import { BacklogTable, Backlog } from "./_components/backlog-table";
+import { fetchIssuesFromProject } from "@/utils/linear";
 
 export default function BacklogPage() {
+  const [data, setData] = useState<Backlog[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const issues = await fetchIssuesFromProject(
+        process.env.NEXT_PUBLIC_LINEAR_PROJECT_ID || "",
+      );
+
+      const formattedData = issues.map((issue) => ({
+        id: issue.id,
+        title: issue.title,
+        description: issue.description || "",
+        status: issue.priorityLabel || "",
+      }));
+
+      setData(formattedData);
+    };
+    fetchData();
+  }, []);
+
   return (
     <main className="md:px-10 py-20 flex flex-col gap-24">
       <div className="flex flex-col items-start md:items-center gap-y-6 md:gap-y-2">
@@ -12,7 +36,7 @@ export default function BacklogPage() {
           Backlog
         </GradientText>
       </div>
-      <BacklogTable />
+      <BacklogTable data={data} />
     </main>
   );
 }
--- src/utils/linear/index.tsx
+++ src/utils/linear/index.tsx
@@ -1,7 +1,7 @@
 import { LinearClient } from "@linear/sdk";
 
 const linearClient = new LinearClient({
-  apiKey: process.env.LINEAR_API_KEY!,
+  apiKey: process.env.NEXT_PUBLIC_LINEAR_API_KEY || "",
 });
 
 export async function fetchIssuesFromProject(projectKey: string) {
@@ -10,6 +10,18 @@ export async function fetchIssuesFromProject(projectKey: string) {
   if (!project) {
     throw new Error(`Project with key "${projectKey}" not found.`);
   }
-  const { nodes } = await project.issues();
+  const { nodes } = await project.issues({
+    first: 100,
+    filter: {
+      state: {
+        type: { in: ["backlog", "unstarted", "in-progress"] },
+      },
+    },
+  });
+  return nodes;
+}
+
+export async function fetchAllProjects() {
+  const { nodes } = await linearClient.projects();
   return nodes;
 }
